a9fb02b1ab4efee772b1902c50a628711528263f
Teohan Ekşi <>
2022-08-27 17:06:18 +0300

    readme and app updated

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hello World!'
+    output = b'<p>Hello Worlds!</p>'
 
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
