ccdf01a8e6ac777135a80053ba857c09f57a077f
Teohan Ekşi <teohan.eksi@gmail.com>
2022-08-28 13:33:22 +0300

    created response object

--- application.py
+++ application.py
@@ -1,9 +1,24 @@
+import os
+import sys
+
+script_dir = os.path.dirname( __file__ )
+mymodule_dir = os.path.join( script_dir, 'utils' )
+sys.path.append( mymodule_dir )
+from functions import get_environ_vars
+
 def application(environ, start_response):
+    response = Response(environ)
+
+    start_response(response.status, response.response_headers)
+    return response.get_output()
+class Response:
+    def __init__(self, environ):
+        self.environ = environ
+
     status = '200 OK'
-    output = b'<p>Hello Worlds!</p>'
 
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-
-    return [output]
+    response_headers = [('Content-type', 'text/plain')]
+    
+    def get_output(self):
+        output = bytes(get_environ_vars(self.environ), "utf-8")
+        return [output] 
