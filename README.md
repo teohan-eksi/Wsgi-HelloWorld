8415a18f5195fb3ccfb9349a0e6895da6ec4ae0c
Teohan Ekşi <teohan.eksi@gmail.com>
2022-12-27 22:36:40 +0300

    implementation of GET and POST methods

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-simple wsgi application
+simple wsgi implementation
 
 an apache server with wsgi module need to be installed.
 
@@ -6,3 +6,5 @@ these two are great starting points:
  
 https://httpd.apache.org/docs/2.4/getting-started.html
 https://modwsgi.readthedocs.io/en/develop/getting-started.html
+
+this program simply implements GET and POST methods with wsgi.
--- application.py
+++ application.py
@@ -6,11 +6,24 @@ mymodule_dir = os.path.join( script_dir, 'utils' )
 sys.path.append( mymodule_dir )
 from functions import get_environ_vars
 
+"""
+The application interface is a callable object
+
+It accepts two arguments:
+    * environ points to a dictionary containing CGI like environment
+    variables which is populated by the server for each received request from the client
+    * start_response is a callback function supplied by the server
+    which takes the HTTP status and headers as arguments
+"""
 def application(environ, start_response):
     response = Response(environ)
-
+    
     start_response(response.status, response.response_headers)
-    return response.get_output()
+    return response.get_body()
+
+# this is my response object that consists of 
+# status, response headers and a function 
+# returning a response body according to the request method
 class Response:
     def __init__(self, environ):
         self.environ = environ
@@ -19,6 +32,18 @@ class Response:
 
     response_headers = [('Content-type', 'text/plain')]
     
-    def get_output(self):
-        output = bytes(get_environ_vars(self.environ), "utf-8")
-        return [output] 
+    def get_body(self):
+        # body = bytes(get_environ_vars(self.environ), "utf-8")
+        body = bytes(request_method_handler(self.environ), "utf-8")
+        return [body] 
+
+def request_method_handler(environ):
+    res = "empty response"
+    if(environ["REQUEST_METHOD"] == "GET"):
+        res = str(environ["QUERY_STRING"])
+        if len(res) == 0:
+            res = "no query"
+    elif(environ["REQUEST_METHOD"] == "POST"):
+        res = str(environ["wsgi.input"].read(int(environ["CONTENT_LENGTH"])))
+
+    return res
